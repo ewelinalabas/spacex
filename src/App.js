bd3655e83b2a0c69743847be57cdd4fb7ab5e093
Ewelina Łabas <ewelina.labas@gmail.com>
2020-01-25 16:55:32 +0100

    Add launch information

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Launch } from './Launch';
 
-function App() {
+export const App = () => {
+  const [pastLaunches, setPastLaunches] = useState([])
+
+  useEffect(() => {
+      axios.get('https://api.spacexdata.com/v3/launches/past?limit=10&order=desc')
+      .then(response => {
+        setPastLaunches(response.data)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Past launches</h3>
+      <ul>
+        {pastLaunches.map(el => (
+          <Launch data={el} key={el.flight_number}/>
+        ))}
+      </ul>
     </div>
-  );
+  )
+  
 }
-
-export default App;
