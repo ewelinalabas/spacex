170953bc6f0878f1cebc84259ecb7e0d6e3378b8
Ewelina Łabas <ewelina.labas@gmail.com>
2020-01-31 13:20:49 +0100

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import Background from './spacex.jpg';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { Card, CardMedia, Button } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
 
-const App = () => {
+const styles = {
+  imgbox: {
+    height: '100%',
+    position: 'relative'
+  },
+  img: {
+    backgroundImage: `url(${Background})`,
+    width: '100%',
+    height: '100vh',
+    margin: 'auto'
+  },
+  overlay: {
+    position: 'absolute',
+    bottom: '30%',
+    left: '25%',
+    color: 'black',
+    backgroundColor: 'white'
+ }
+}
+
+const App = (props) => {
+  const { classes } = props
   return (
-    <h2>Hello</h2>
+    <Card className={classes.imgbox}>
+      <CardMedia 
+        className={classes.img} 
+        alt="'SpaceX Starman' by Florian R. is licensed under CC BY-NC-ND 4.0" >
+           <Button className={classes.overlay} component={NavLink} to={"pastlaunches"}>See past launches</Button>
+      </CardMedia>
+    </Card>
   )
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
--- src/UpcomingLaunch.jsx
+++ src/UpcomingLaunch.jsx
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { convertTimestamp } from './utils';
 
 const styles = {
   card: {
@@ -20,25 +21,27 @@ const styles = {
 const UpcomingLaunch = (props) => {
   const { data } = props
   const { classes } = props
-  
+  const date = convertTimestamp(data.launch_date_unix)
+  const payload = data.rocket.second_stage.payloads[0];
+
   return (
     <div>
       <Grid item xs={6}>
       <Card className={classes.card}>
         <CardActionArea>
-          <CardMedia 
-            component="picture"
+          {/* <CardMedia 
+            component="img"
             className={classes.media}
             title={data.links.flight_number} 
             src={data.links.mission_patch}
             height="315"
-          />
+          /> */}
           <CardContent>
             <Typography variant="h6">{data.mission_name}</Typography>
-            {/* <Typography>Date: {date}</Typography>
+            <Typography>Date: {date}</Typography>
             <Typography>Rocket: {data.rocket.rocket_name}</Typography>
             <Typography>Site: {data.launch_site.site_name_long}</Typography>
-            <Typography>Payload: {payload.payload_type}</Typography> */}
+            <Typography>Payload: {payload.payload_type}</Typography>
           </CardContent>
         </CardActionArea>
       </Card>
